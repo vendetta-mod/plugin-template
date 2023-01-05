258e7b66135e5ff91cc59ca84e3bee6d096b826a
Beefers <Beefers@users.noreply.github.com>
2023-01-05 23:04:27 +0000

    deploy: 90e115fda7a01dc7f2c7c8b223d23b98b7d6bbae

--- test/index.js
+++ test/index.js
@@ -1,1 +1,1 @@
-(function(o){"use strict";function n(){window.vendetta.logger.log("Hello world!")}function d(){window.vendetta.logger.log("Goodbye, world.")}return o.onLoad=n,o.onUnload=d,o})({});
+(function(o){"use strict";const{logger:n}=vendetta;function l(){n.log("Hello world!")}function d(){n.log("Goodbye, world.")}return o.onLoad=l,o.onUnload=d,o})({});
