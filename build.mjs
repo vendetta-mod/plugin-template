db092380cb5def40f25635be883c7ecd5e5775f3
Beef <beefers@riseup.net>
2023-02-26 00:18:51 +0000

    [Build] Tidy

--- build.mjs
+++ build.mjs
@@ -1,10 +1,11 @@
-import { rollup } from "rollup";
 import { readFile, writeFile, readdir } from "fs/promises";
 import { createHash } from "crypto";
-import nodeResolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
+
+import { rollup } from "rollup";
 import { swc } from "rollup-plugin-swc3";
 import esbuild from "rollup-plugin-esbuild";
+import commonjs from "@rollup/plugin-commonjs";
+import nodeResolve from "@rollup/plugin-node-resolve";
 
 for (let plug of await readdir("./plugins")) {
     const manifest = JSON.parse(await readFile(`./plugins/${plug}/manifest.json`));
