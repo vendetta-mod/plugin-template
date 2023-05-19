1fb12dbb3d326467db7754691b1eab32d136ee5a
hmm <60791542+hmm-monke@users.noreply.github.com>
2023-05-20 00:04:45 +0500

    Update index.ts

--- plugins/NiceExperiments/src/index.ts
+++ plugins/NiceExperiments/src/index.ts
@@ -2,5 +2,6 @@ export const onLoad = () => {
  
 vendetta.metro.findByProps("ChatListAnimationExperiment").ChatListAnimationExperiment.getCurrentConfig().shouldAnimateAndroid = true;
 vendetta.metro.findByProps("MultiAccountMobileExperiment").MultiAccountMobileExperiment.getCurrentConfig().isMultiAccountMobileEnabled = true;
+vendetta.metro.findByProps("useIOSDiscoveryExperiment").default.getCurrentConfig().showDiscovery = true;
 
 }
